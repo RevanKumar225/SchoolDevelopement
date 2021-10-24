93ee61a3c1f25296ff7e8cc3d5106de7e20d000d
Revan <revan.chary@gmail.com>
2021-10-24 15:12:20 +0530

    WebTeacherModule

---
+++ SchoolIn/src/app/feature/web-teacher/web-teacher.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { WebTeacherRoutingModule } from './web-teacher-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    WebTeacherRoutingModule
+  ]
+})
+export class WebTeacherModule { }
