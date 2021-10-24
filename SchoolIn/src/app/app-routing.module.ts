2d5764c118a5fffbc6414850f75ea0b9d3ffd8ef
Revan <revan.chary@gmail.com>
2021-10-24 15:23:56 +0530

    WebTeacherLazyLoad

--- SchoolIn/src/app/app-routing.module.ts
+++ SchoolIn/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'feature', pathMatch: 'full' },
+  { path: 'feature', loadChildren: () => import('./feature/web-teacher/web-teacher.module').then(m => m.WebTeacherModule)},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
